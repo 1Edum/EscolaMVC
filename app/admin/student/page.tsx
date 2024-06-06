965561fadbf0badea70a74207511d137827bb008
1Edum <eduardomatosdesouza2@gmail.com>
2024-06-06 15:51:47 -0300

    sheat: add shadcn/ui card and stylization

--- app/admin/student/page.tsx
+++ app/admin/student/page.tsx
@@ -1,16 +1,17 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import InputCadastro from "../_components/input-cadastro";
-import useStudentCount from "../../hooks/useStudentCount";
+import useStudentCount from "../../_helpers/hooks/useStudentCount";
 import {
   Card,
   CardDescription,
   CardHeader,
   CardTitle,
 } from "@/app/_components/ui/card";
-import useRoomCount from "../../hooks/useRoomCount";
-import { Users2Icon } from "lucide-react";
+import useRoomCount from "../../_helpers/hooks/useRoomCount";
+import { Users2Icon, School2Icon } from "lucide-react";
+import variationStudent from "@/app/_helpers/variationStudent";
 
 const studentFields = [
   { name: "name", type: "text", placeholder: "Name", required: true },
@@ -27,28 +28,45 @@ const studentFields = [
 function Page() {
   const studentCount = useStudentCount();
   const roomCount = useRoomCount();
+  const [percentageLastMonth, setPercentageLastMonth] = useState<number | null>(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const percentage = await variationStudent();
+        setPercentageLastMonth(percentage);
+      } catch (error) {
+        console.error('Error fetching student statistics:', error);
+      }
+    }
+
+    fetchData();
+  }, []);
   const cardsConfig = [
-    { title: "Quantidade de Alunos Cadastrados", description: studentCount },
-    { title: "Quantidade de Salas Cadastradas", description: roomCount },
+    { title: "Number of Registered Students", description: studentCount, icon:<Users2Icon /> },
+    { title: "Quantidade de Salas Cadastradas", description: roomCount, icon:<School2Icon />  },
   ];
   return (
     <main className="flex flex-col gap-y-2">
       <section className="flex flex-col md:flex-row w-full justify-around  gap-y-2">
         {cardsConfig.map((card) => (
           <Card
-            className="px-5 py-3 flex md:flex-col justify-between"
+            className="p-5 flex md:flex-col justify-between bg-red-700 border-none md:w-1/3"
             key={card.title}
           >
-            <CardHeader className="p-0 flex flex-row items-center">
-              <CardTitle className="text-lg ">
+            <CardHeader className="p-0 flex flex-row items-center gap-1 md:gap-x-10">
+              <CardTitle className="text-sm w-5/6 md:w-full text-slate-200">
                 {card.title}
               </CardTitle>
-              <div>
-                <Users2Icon />
+              <div className="text-slate-200 flex items-center justify-center">
+                {card.icon}          
               </div>
             </CardHeader>
-            <CardDescription className="text-xl md:mt-3">
-              <h3>{card.description}</h3>
+            <CardDescription className="md:mt-3 text-3xl text-white font-extrabold">
+              {card.description}
+            </CardDescription>
+            <CardDescription className="text-sm hidden md:flex text-slate-200">
+              {percentageLastMonth} from last month
             </CardDescription>
           </Card>
         ))}
