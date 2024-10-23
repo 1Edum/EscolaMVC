dda19d785617552a16680760cb406c89d168ca91
1Edum <eduardomatosdesouza2@gmail.com>
2024-10-23 15:06:39 -0300

    feat: add header component in layout-admin

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,12 +1,12 @@
-import Sidebar from "./_components/sidebar";
+import { Header } from "../_components/header"
 
- 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
-      <div className="w-full flex-none md:w-64">
-        <Sidebar />
-      </div>
+     <Header variant={"admin"}>
+        <h1>Header</h1>
+     </Header>
+        
       <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
     </div>
   );
