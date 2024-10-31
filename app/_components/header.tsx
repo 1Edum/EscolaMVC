b16e6502c586977b4e6fe3fc06ea3fd948ee6fc1
1Edum <eduardomatosdesouza2@gmail.com>
2024-10-31 09:31:01 -0300

    feat: create header-links component

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,11 +1,13 @@
+
 import { cn } from "@/lib/utils";
 import { cva, type VariantProps } from "class-variance-authority";
+import Link from "next/link";
 import React from "react";
 
-const headerVariants = cva("flex items-center", {
+const headerVariants = cva("flex w-full items-center", {
   variants: {
     variant: {
-      admin: "bg-primary h-10",
+      admin: "bg-primary h-[12vh] justify-between",
     },
   },
 });
@@ -19,11 +21,31 @@ const Header = React.forwardRef<HTMLHeadingElement, HeaderProps>(({ variant, asC
   <header
     ref={ref}
     className={cn(headerVariants({variant})
-
     )}
     {...props}
   />
 ));
 Header.displayName = "Header"
 
-export {Header}
+interface LinkItem{
+  name: string;
+  link: string;
+}
+
+interface HeaderLinksProps {
+  links?: LinkItem[]; 
+}
+
+const HeaderLinks = React.forwardRef<HTMLUListElement, HeaderLinksProps>(({ links }, ref) => (
+  
+  <ul className="flex items-center" ref={ref}>
+    {links?.map((link) => (
+      <Link key={link.name} href={link.link}>
+        {link.name}
+      </Link>
+    ))}
+  </ul>
+));
+HeaderLinks.displayName = "HeaderLinks"
+
+export {Header, HeaderLinks}
